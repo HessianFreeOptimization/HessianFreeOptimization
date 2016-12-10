6b2699cd0db3436824469e78ba8f93a1a6ea4554
Yifeng Tao <taoyf12@sina.com>
2016-12-10 06:41:17 -0500

    realize lbfgs

--- SGD_minist.m
+++ SGD_minist.m
@@ -36,7 +36,7 @@ initial_nn_params = [initial_Theta1(:) ; initial_Theta2(:)];
 fprintf('Training Neural Network... \n')
 
 %options = optimset('MaxIter', 10);
-options.MaxIter = 100;
+options.MaxIter = 300;
 options.HessUpdate = 'lbfgs';
 %options.HessUpdate = 'steepdesc';
 options.Display = 'iter';
@@ -52,16 +52,102 @@ costFunction = @(p) nnCostFunction(p, ...
                                    num_labels, X, y, lambda);
 
                                
-[nn_params, cost] = fminlbfgs( costFunction, initial_nn_params, options);
-                               
-                               
-% Now, costFunction is a function that takes in only one argument (the
-% neural network parameters)
-%[nn_params, cost] = fmincg(costFunction, initial_nn_params, options);
+%[nn_params, cost] = fminlbfgs( costFunction, initial_nn_params, options);
 
-%[nn_params, cost] = fminunc(costFunction, initial_nn_params, options);
+x = initial_nn_params;
+m = 7;
+l = size(x,1);
+
+cost = [];
+
+s = [];
+y = [];
+[fval, grad] = costFunction(x);
+
+for k = 1:100
+    cost = [cost, fval];
+    fprintf('%d\t%.5f\n',k-1,fval)
+    %[xn,v,mt,vt,diagG,Eg2,Et2] = gradupdate(strategy,grad,x,xoo,v,diagG,Eg2,Et2,...
+    %    mt,vt,i,eta,gamma,beta1,beta2,epsilon);
+    
+    % s: l x m, Delta_position
+    % y: l x m, Delta_grad
+	% pk = L_BFGS(g,s,y,m,Hdiag);
+    
+    q = -grad;
+    p = q;
+    if k ~= 1
+        alpha = zeros(1,k);
+        for i = size(s,2):-1:1
+%             if s(:,i)'*q == 0
+%                 alpha(i) = 0;
+%             else
+                alpha(i) = s(:,i)'*q / (y(:,i)'*s(:,i));
+%             end
+            q = q - alpha(i)*y(:,i);
+        end
+%         if y(:,k-1)'*y(:,k-1) == 0
+%         	H0 = zeros(l);
+%         else
+            H0 = y(:,end)'*s(:,end) / (y(:,end)'*y(:,end)) * eye(l);
+%         end
+        p = H0*q;
+        %max(q)
+        %max(p)
+        for i = 1:size(s,2)
+%             if y(:,i)'*p == 0
+%                 beta = 0;
+%             else
+                beta = y(:,i)'*p / (y(:,i)'*s(:,i));
+%             end
+            p = p + (alpha(i) - beta)*s(:,i);
+        end
+        %max(p)
+    end
+
+    t = 1;
+    while costFunction(x+t*p) > costFunction(x) + 0.0001*t*grad'*p
+       t = 0.9*t; 
+    end
+    
+    s = [s, t*p];
+    x = x + t*p;
+    grado = grad;
+    [fval, grad] = costFunction(x);
+    y = [y, grad - grado];
+    
+    if size(s,2) > m
+        s = s(:,2:end);
+        y = y(:,2:end);
+    end
+end
+nn_params = x;
+plot(cost);
 
 
+
+
+
+
+
+
+% nn_params = initial_nn_params;
+% [fval, grad] = costFunction(nn_params);
+% eta = 2;
+% cost = fval;
+% fprintf('%d\t%.5f\n',0,fval)
+% for iter = 1:100
+%    nn_params = nn_params - eta*grad;
+%    [fval,grad] = costFunction(nn_params);
+%    fprintf('%d\t%.5f\n',iter,fval)
+%    cost = [cost, fval];
+% end
+% 
+% 
+% 
+% plot(cost);
+figure('color', [1 1 1]);
+plot(0:length(cost)-1,cost);
 
 
 % Obtain Theta1 and Theta2 back from nn_params
@@ -76,11 +162,11 @@ pred = predict(Theta1, Theta2, X);
 
 fprintf('Training Set Accuracy: %f\n', mean(double(pred == y)) * 100);
 loss = cost;
-save('loss_lbfgs.mat', 'loss');
+%save('loss_gd.mat', 'loss');
 
 
 %%
-load('loss_lbfgs.mat');
+load('loss_gd.mat');
 figure('color', [1 1 1]);
 plot(0:length(loss)-1,loss);
 
