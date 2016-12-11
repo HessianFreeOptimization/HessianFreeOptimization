fe0ef6940ba2703a40620a000fc0174ecda98684
Jerrypiglet <jerrypiglet@gmail.com>
2016-12-11 15:56:32 -0500

    Rui-combine_adam_HF

--- gd_train.m
+++ gd_train.m
@@ -1,4 +1,4 @@
-function [llrecord, errrecord] = gd_train(algorithm, maxIter, layersizes, layertypes)
+function [llrecord, errrecord, paramsp] = gd_train(algorithm, maxIter, layersizes, layertypes, paramsinit)
 % variables
 llrecord = zeros(maxIter+1,2);
 errrecord = zeros(maxIter+1,2);
@@ -129,17 +129,21 @@ times = zeros(maxIter,1);
 totalpasses = 0;
 
 % initialization of params.
-paramsp = zeros(psize,1);
-[Wtmp,btmp] = unpack(paramsp);
-numconn = 15;
-for i = 1:numlayers
-    initcoeff = 1;
-    for j = 1:layersizes(i+1)
-        idx = ceil(layersizes(i)*rand(1,numconn));
-        Wtmp{i}(j,idx) = randn(numconn,1)*initcoeff;
+if nargin == 4
+    paramsp = zeros(psize,1);
+    [Wtmp,btmp] = unpack(paramsp);
+    numconn = 15;
+    for i = 1:numlayers
+        initcoeff = 1;
+        for j = 1:layersizes(i+1)
+            idx = ceil(layersizes(i)*rand(1,numconn));
+            Wtmp{i}(j,idx) = randn(numconn,1)*initcoeff;
+        end
     end
+    paramsp = pack(Wtmp, btmp);
+else
+    paramsp = paramsinit;
 end
-paramsp = pack(Wtmp, btmp);
 
 function grad = calcu_grad(paramsp)
     [Wu, bu] = unpack(paramsp);
