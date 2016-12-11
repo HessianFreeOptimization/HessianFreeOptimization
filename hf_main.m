613cef6b0bae97fe1805d5189258a565651bbb94
Yifeng Tao <taoyf12@sina.com>
2016-12-11 16:39:16 -0500

    change dataset to whole mnist

--- hf_main.m
+++ hf_main.m
@@ -1,24 +1,37 @@
 clc; clear; close all;
-maxIter = 100;
+maxIter = 20;
 % network structure
 layersizes = [25 30];
 layertypes = {'logistic', 'logistic', 'softmax'};
 
-[llrecord, errrecord] = hf_train(maxIter, layersizes, layertypes);
+% load datasets.
+indata = loadMNISTImages('raw_data/train-images.idx3-ubyte');
+y = loadMNISTLabels('raw_data/train-labels.idx1-ubyte');
+y(y==0) = 10;
+intest = loadMNISTImages('raw_data/t10k-images.idx3-ubyte');
+yt = loadMNISTLabels('raw_data/t10k-labels.idx1-ubyte');
+yt(yt==0) = 10;
 
-%%
+outdata = zeros(10,size(indata,2));
+for i = 1:10
+	outdata(i,:) = (y == i);
+end
+
+outtest = zeros(10,size(intest,2));
+for i = 1:10
+	outtest(i,:) = (yt == i);
+end
+
+[llrecord, errrecord] = hf_train(maxIter, layersizes, layertypes, indata, outdata, intest, outtest);
+
 fig = figure('color', [1 1 1]);
 subplot(1,2,1);
 plot(0:maxIter, -llrecord(:,1),'rx-');
 hold on;
 plot(0:maxIter, -llrecord(:,2),'bx-');
-title('log-likelihood');
-
 subplot(1,2,2);
 plot(0:maxIter, errrecord(:,1),'rx-');
 hold on;
 plot(0:maxIter, errrecord(:,2),'bx-');
-title('error');
-
 ylim([0 1]);
 set(fig, 'Position', [10, 10, 1000, 400]);
