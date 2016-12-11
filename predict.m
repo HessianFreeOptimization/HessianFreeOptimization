7acf60d7adaa624e830f8bcdb5e119ab411077df
Yifeng Tao <taoyf12@sina.com>
2016-12-10 21:08:47 -0500

    Delete predict.m

--- predict.m
+++
@@ -1,6 +0,0 @@
-function p = predict(Theta1, Theta2, X)
-    m = size(X, 1);
-    h1 = sigmoid([ones(m, 1) X] * Theta1');
-    h2 = sigmoid([ones(m, 1) h1] * Theta2');
-    [~, p] = max(h2, [], 2);
-end
