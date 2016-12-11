511b59880b8332b892608b5b5fa2a4a1a6613dbc
Jerrypiglet <jerrypiglet@gmail.com>
2016-12-11 17:31:12 -0500

    Rui-univeral plot func

--- plot_curve.m
+++ plot_curve.m
@@ -2,17 +2,17 @@ function [ ] = plot_curve( llrecord,  errrecord)
     iters = size(llrecord, 1) - 1;
     fig = figure('color', [1 1 1]);
     subplot(1,2,1);
-    semilogy(0:iters, -llrecord(:,1),'rx-');
+    semilogy(0:iters, -llrecord(:,1),'r-');
     ylim([0 max(-llrecord(:,1))]);
     hold on;
-    semilogy(0:iters, -llrecord(:,2),'bx-');
+    semilogy(0:iters, -llrecord(:,2),'b-');
     ylim([0 max(-llrecord(:,2))]);
     title('log-likelihood');
 
     subplot(1,2,2);
-    plot(0:iters, errrecord(:,1),'rx-');
+    plot(0:iters, errrecord(:,1),'r-');
     hold on;
-    plot(0:iters, errrecord(:,2),'bx-');
+    plot(0:iters, errrecord(:,2),'b-');
     title('error');
     ylim([0 1]);
     set(fig, 'Position', [10, 10, 1000, 400]);
