3b34c7436eec65ecc3fd04b7fa17df5e74525666
Jerrypiglet <jerrypiglet@gmail.com>
2016-12-11 18:30:48 -0500

    Rui-params

--- plot_curve.m
+++ plot_curve.m
@@ -2,10 +2,10 @@ function [ ] = plot_curve( llrecord,  errrecord)
     iters = size(llrecord, 1) - 1;
     fig = figure('color', [1 1 1]);
     subplot(1,2,1);
-    semilogy(0:iters, -llrecord(:,1),'r-', 'MarkerSize', 3);
+    plot(0:iters, -llrecord(:,1),'r-', 'MarkerSize', 3);
     ylim([0 max(-llrecord(:,1))]);
     hold on;
-    semilogy(0:iters, -llrecord(:,2),'b-');
+    plot(0:iters, -llrecord(:,2),'b-');
     ylim([0 max(-llrecord(:,2))]);
     title('log-likelihood');
 
