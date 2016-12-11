7d0bed50af44fd3aebc9378876d338e0b87a8674
Yifeng Tao <taoyf12@sina.com>
2016-12-11 16:59:19 -0500

    change to whole mnist

--- gd_main.m
+++ gd_main.m
@@ -1,6 +1,9 @@
 clc; clear; close all;
 maxIter = 100;
 
+
+
+
 % network structure
 layersizes = [25 30];
 layertypes = {'logistic', 'logistic', 'softmax'};
@@ -8,7 +11,23 @@ algorithm = {'gradient descent','momentum','nesterov accelerated gradient',...
     'adagrad','RMSprop','adadelta','adam'};
 algorithm = algorithm{7};
 
-[llrecord, errrecord] = gd_train(algorithm, maxIter, layersizes, layertypes);
+% load datasets.
+indata = loadMNISTImages('raw_data/train-images.idx3-ubyte');
+y = loadMNISTLabels('raw_data/train-labels.idx1-ubyte');
+y(y==0) = 10;
+intest = loadMNISTImages('raw_data/t10k-images.idx3-ubyte');
+yt = loadMNISTLabels('raw_data/t10k-labels.idx1-ubyte');
+yt(yt==0) = 10;
+outdata = zeros(10,size(indata,2));
+for i = 1:10
+	outdata(i,:) = (y == i);
+end
+outtest = zeros(10,size(intest,2));
+for i = 1:10
+	outtest(i,:) = (yt == i);
+end
+
+[llrecord, errrecord] = gd_train(algorithm, maxIter, layersizes, layertypes, indata, outdata, intest, outtest);
 
 fig = figure('color', [1 1 1]);
 subplot(1,2,1);
