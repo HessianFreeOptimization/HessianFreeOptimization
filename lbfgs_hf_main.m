796cb4d40068c1d758abe91e95e2cac93c5625f8
Jerrypiglet <jerrypiglet@gmail.com>
2016-12-11 17:04:40 -0500

    Rui-combine_adam_new_interface

--- lbfgs_hf_main.m
+++ lbfgs_hf_main.m
@@ -1,11 +1,30 @@
 clc; clear; close all;
 maxIter = 100;
-% network structure
+%% network structure
 layersizes = [25 30];
 layertypes = {'logistic', 'logistic', 'softmax'};
 
-[llrecord, errrecord, params] = gd_train('adam', 150, layersizes, layertypes);
-[llrecord2, errrecord2, ~] = lbfgs_train(60, layersizes, layertypes, params);
+%% load datasets.
+indata = loadMNISTImages('raw_data/train-images.idx3-ubyte');
+y = loadMNISTLabels('raw_data/train-labels.idx1-ubyte');
+y(y==0) = 10;
+intest = loadMNISTImages('raw_data/t10k-images.idx3-ubyte');
+yt = loadMNISTLabels('raw_data/t10k-labels.idx1-ubyte');
+yt(yt==0) = 10;
+
+outdata = zeros(10,size(indata,2));
+for i = 1:10
+	outdata(i,:) = (y == i);
+end
+
+outtest = zeros(10,size(intest,2));
+for i = 1:10
+	outtest(i,:) = (yt == i);
+end
+
+%% training
+[llrecord, errrecord, params] = gd_train('adam', 150, layersizes, layertypes, indata, outdata, intest, outtest);
+[llrecord2, errrecord2, ~] = lbfgs_train(60, layersizes, layertypes, params, indata, outdata, intest, outtest);
 % [llrecord2, errrecord2, ~] = hf_train(60, layersizes, layertypes, params);
 llrecord = [llrecord; llrecord2];
 errrecord = [errrecord; errrecord2];
