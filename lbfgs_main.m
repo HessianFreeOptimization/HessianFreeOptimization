85bfa1f0f4907264a4b0252f40be0d43e3a57411
Jerrypiglet <jerrypiglet@gmail.com>
2016-12-10 22:19:07 -0500

    Rui-13

--- lbfgs_main.m
+++ lbfgs_main.m
@@ -12,9 +12,13 @@ subplot(1,2,1);
 plot(0:maxIter, -llrecord(:,1),'rx-');
 hold on;
 plot(0:maxIter, -llrecord(:,2),'bx-');
+title('log-likelihood');
+
 subplot(1,2,2);
 plot(0:maxIter, errrecord(:,1),'rx-');
 hold on;
 plot(0:maxIter, errrecord(:,2),'bx-');
+title('error');
+
 ylim([0 1]);
 set(fig, 'Position', [10, 10, 1000, 400]);
