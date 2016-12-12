8f1bc0b0a8998ca64c0f0ad424d50a60da4fc996
Jerrypiglet <jerrypiglet@gmail.com>
2016-12-12 00:11:09 -0500

    Rui-count_evals

--- plot_curve.m
+++ plot_curve.m
@@ -1,27 +1,42 @@
-function [ ] = plot_curve(llrecord,  errrecord, eval_s)
-    if nargin == 2
-        x_inds = 0:iters;
-    else
-        x_inds = eval_s;
-    end
-    iters = size(llrecord, 1) - 1;
-    fig = figure('color', [1 1 1]);
+function [ ] = plot_curve(records, fig)
+    fig;
     subplot(1,2,1);
-    semilogy(x_inds, -llrecord(:,1),'r-', 'MarkerSize', 3);
-    ylim([0 max(-llrecord(:,1))]);
-    hold on;
-    semilogy(x_inds, -llrecord(:,2),'b-');
+    max_llrecord = 0;
+    for trial = 1:length(records)
+        iters = size(records{trial}.llrecord, 1) - 1;
+        if nargin == 2
+            x_inds = 0:iters;
+        else
+            x_inds = eval_s;
+        end
+        semilogy(x_inds, -records{trial}.llrecord(:,1),'r-', 'MarkerSize', 3);
+        hold on;
+        semilogy(x_inds, -records{trial}.llrecord(:,2),'b-');
+        hold on;
+        if max(max(-records{trial}.llrecord)) > max_llrecord
+            max_llrecord = max(max(-records{trial}.llrecord));
+        end
+    end
     grid on;
-    ylim([0 max(-llrecord(:,2))]);
-    title('log-likelihood');
-
+    max_llrecord
+    ylim([0, max_llrecord]);
+    title('objective');
+    
     subplot(1,2,2);
-    semilogy(x_inds, errrecord(:,1),'r-');
-    hold on;
-    semilogy(x_inds, errrecord(:,2),'b-');
+    for trial = 1:length(records)
+        iters = size(records{trial}.llrecord, 1) - 1;
+        if nargin == 2
+            x_inds = 0:iters;
+        else
+            x_inds = eval_s;
+        end
+        records{trial}.errrecord
+        semilogy(x_inds, records{trial}.errrecord(:,1),'r-');
+        hold on;
+        semilogy(x_inds, records{trial}.errrecord(:,2),'b-');
+        hold on;
+    end
     grid on;
-    title('error');
+    title('train/test error');
     ylim([0 1]);
-    set(fig, 'Position', [10, 10, 1000, 400]);
-
 end
--- single_main.m
+++ single_main.m
@@ -19,18 +19,36 @@ outtest = zeros(10,size(intest,2));
 for i = 1:10
 	outtest(i,:) = (yt == i);
 end
+% params.indata = indata(:, 1000);
+% params.outdata = outdata(:, 1000);
+% params.intest = intest(:, 1000);
+% params.outtest = outtest(:, 1000);
 params.indata = indata;
 params.outdata = outdata;
 params.intest = intest;
 params.outtest = outtest;
-global eval_f;
-eval_f = 0;
-global eval_g;
-eval_g = 0;
-
 %% training
-iters = 4000;
-[llrecord, errrecord, weights, eval_fs, eval_gs] = gd_train('adam', iters, params);
+iters = 4;
+trials = 5;
+algorithm = 'adam';
+records = cell(trials, 1);
+for trial = 1:10
+    global eval_f;
+    eval_f = 0;
+    global eval_g;
+    eval_g = 0;
+    [llrecord, errrecord, weights, eval_fs, eval_gs] = gd_train(algorithm, iters, params);
+    records{trial}.llrecord = llrecord;
+    records{trial}.errrecord = errrecord;
+    records{trial}.weights = weights;
+    records{trial}.eval_fs = eval_fs;
+    records{trial}.eval_gs = eval_gs;
+end
+save(sprintf('./saved/single_%s_for_%d_iter.mat', algorithm, iters), 'records');
 % [llrecord, errrecord, weights, eval_fs, eval_gs] = hf_train(iters, params, weights);
-%%
-plot_curve(llrecord,  errrecord, eval_fs);
+
+%% plotting
+% fig1 = figure('color', [1 1 1]);
+% set(fig1, 'Position', [10, 10, 1000, 400]);
+fig1 = figure(1);
+plot_curve(records, fig1);
