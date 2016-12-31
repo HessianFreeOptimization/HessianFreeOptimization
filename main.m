cf587c897dd5074e119395416b9d8a0c57ea0b08
Yifeng Tao <taoyf12@sina.com>
2016-12-31 01:32:14 -0500

    gd w/ backtracking

---
+++ main.m
@@ -0,0 +1,76 @@
+clc; clear; close all;
+% network structure
+% TODO:
+params.layersizes = [100 30];
+params.layertypes = {'logistic', 'logistic', 'softmax'};
+params.weight_decay = 2e-4;
+
+% load datasets.
+indata = loadMNISTImages('raw_data/train-images.idx3-ubyte');
+y = loadMNISTLabels('raw_data/train-labels.idx1-ubyte');
+y(y==0) = 10;
+intest = loadMNISTImages('raw_data/t10k-images.idx3-ubyte');
+yt = loadMNISTLabels('raw_data/t10k-labels.idx1-ubyte');
+yt(yt==0) = 10;
+outdata = zeros(10,size(indata,2));
+for i = 1:10
+	outdata(i,:) = (y == i);
+end
+outtest = zeros(10,size(intest,2));
+for i = 1:10
+	outtest(i,:) = (yt == i);
+end
+params.indata = indata(:, 1:20000);
+params.outdata = outdata(:, 1:20000);
+params.intest = intest(:, 1:10000);
+params.outtest = outtest(:, 1:10000);
+
+% training
+% iters = 6000;
+% trials = 10;
+iters = 6000;
+trials = 10;
+global eval_f;
+global eval_g;
+
+algorithms = {'gradient descent', 'fixstep-lbfgs', 'momentum-lbfgs', 'lbfgs'};
+algorithm = algorithms{1};
+
+%for eta = [0.002, 0.005, 0.01, 0.02, 0.05, 0.1, 0.2]
+for eta = 1
+    records = cell(trials, 1);
+    if strcmp(algorithm, 'gradient descent')
+        logfile = sprintf('./saved/gd-for-%d_iters-%d_trials-%.4f_eta.log', iters, trials, eta);
+    elseif strcmp(algorithm, 'lbfgs')
+        logfile = sprintf('./saved/lbfgs-for-%d_iters-%d_trials-%.4f_eta.log', iters, trials, eta);
+    elseif strcmp(algorithm, 'fixstep-lbfgs')
+        logfile = sprintf('./saved/fixstep-lbfgs-for-%d_iters-%d_trials-%.4f_eta.log', iters, trials, eta);
+    elseif strcmp(algorithm, 'momentum-lbfgs')
+        logfile = sprintf('./saved/momentum-lbfgs-for-%d_iters-%d_trials-%.4f_eta.log', iters, trials, eta);
+    end
+    fid = fopen(logfile, 'w');
+    for trial = 1:trials
+        eval_f = 0;
+        eval_g = 0;
+        params.trial = trial;       
+        [llrecord, errrecord, weights, eval_fs, eval_gs, step_size] = optimization_method(algorithm, eta, iters, fid, params);
+        records{trial}.llrecord = llrecord;
+        records{trial}.errrecord = errrecord;
+        records{trial}.weights = weights;
+        records{trial}.eval_fs = eval_fs;
+        records{trial}.eval_gs = eval_gs;
+        records{trial}.step_size = step_size;
+    end
+    fclose(fid);
+    if strcmp(algorithm, 'gradient descent')
+        save(sprintf('./saved/gd-for-%d_iters-%d_trials-%.4f_eta.mat', iters, trials, eta), 'records');
+    elseif strcmp(algorithm, 'lbfgs')
+        save(sprintf('./saved/lbfgs-for-%d_iters-%d_trials-%.4f_eta.mat', iters, trials, eta), 'records');
+    elseif strcmp(algorithm, 'fixstep-lbfgs')
+        save(sprintf('./saved/fixstep-lbfgs-for-%d_iters-%d_trials-%.4f_eta.mat', iters, trials, eta), 'records');
+    elseif strcmp(algorithm, 'momentum-lbfgs')
+        save(sprintf('./saved/momentum-lbfgs-for-%d_iters-%d_trials-%.4f_eta.mat', iters, trials, eta), 'records');
+    end
+end
+
+%EOF.
---
+++ optimization_method.m
@@ -0,0 +1,293 @@
+function [llrecord, errrecord, paramsp, eval_fs, eval_gs, step_size] = optimization_method(algorithm, eta, maxIter, fid, params, paramsinit)
+
+function outputString(s)
+    fprintf( '%s\n', s );
+    fprintf(fid, '%s\n', s);
+end
+
+function v = vec(A)
+    v = A(:);
+end
+
+% variables
+llrecord = zeros(maxIter+1,2);
+errrecord = zeros(maxIter+1,2);
+eval_fs = zeros(maxIter+1,1);
+eval_gs = zeros(maxIter+1,1);
+step_size = zeros(maxIter,1);
+global eval_f;
+global eval_g;
+
+%standard L_2 weight-decay and params:
+weight_decay = params.weight_decay;
+layersizes = params.layersizes;
+layertypes = params.layertypes;
+indata = params.indata;
+outdata = params.outdata;
+intest = params.intest;
+outtest = params.outtest;
+
+layersizes = [size(indata,1) layersizes size(outdata,1)];
+numlayers = size(layersizes,2) - 1;
+
+[indims, numcases] = size(indata);
+outputString(['input size:' num2str(indims) 'x' num2str(numcases)])
+
+y = cell(1, numlayers+1);
+
+psize = layersizes(1,2:(numlayers+1))*layersizes(1,1:numlayers)' + sum(layersizes(2:(numlayers+1)));
+
+% pack all the parameters into a single vector
+function M = pack(W,b)
+    M = zeros( psize, 1 );
+    cur = 0;
+    for i = 1:numlayers
+        M((cur+1):(cur + layersizes(i)*layersizes(i+1)), 1) = vec( W{i} );
+        cur = cur + layersizes(i)*layersizes(i+1);
+        M((cur+1):(cur + layersizes(i+1)), 1) = vec( b{i} );
+        cur = cur + layersizes(i+1);
+    end
+end
+
+% unpack parameters from a vector
+function [W,b] = unpack(M)
+    W = cell( numlayers, 1 );
+    b = cell( numlayers, 1 );
+    cur = 0;
+    for i = 1:numlayers
+        W{i} = reshape( M((cur+1):(cur + layersizes(i)*layersizes(i+1)), 1), [layersizes(i+1) layersizes(i)] );
+        cur = cur + layersizes(i)*layersizes(i+1);
+        b{i} = reshape( M((cur+1):(cur + layersizes(i+1)), 1), [layersizes(i+1) 1] );
+        cur = cur + layersizes(i+1);
+    end
+end
+
+% compute value of loss function
+function [ll, err] = computeLL(params, in, out)
+    [W,b] = unpack(params);
+    schunk = size(in,2);
+    
+    yi = in(:, 1:schunk );
+    outc = out(:, 1:schunk );
+    for i = 1:numlayers
+        xi = W{i}*yi + repmat(b{i}, [1 schunk]);
+        if strcmp(layertypes{i}, 'logistic')
+            yi = 1./(1 + exp(-xi));
+        elseif strcmp(layertypes{i}, 'softmax' )
+            tmp = exp(xi);
+            yi = tmp./repmat( sum(tmp), [layersizes(i+1) 1] );   
+        end
+    end
+    eval_f = eval_f + 1;
+
+    ll = 0;
+    if strcmp( layertypes{numlayers}, 'softmax' )
+        ll = sum(sum(outc.*log(yi)));
+    elseif strcmp( layertypes{numlayers}, 'logistic' )
+        ll = sum(sum(xi.*(outc - (xi >= 0)) - log(1+exp(xi - 2*xi.*(xi>=0)))));
+    end
+    
+    err = sum( sum(outc.*yi,1) ~= max(yi,[],1) ) / size(in,2);
+
+    ll = ll / size(in,2);
+    ll = ll - 0.5*weight_decay*params'*params;
+end
+
+if nargin == 5
+    % initialization of params.
+    paramsp = zeros(psize,1);
+    [Wtmp,btmp] = unpack(paramsp);
+    numconn = 15;
+    for i = 1:numlayers
+        initcoeff = 1;
+        for j = 1:layersizes(i+1)
+            idx = ceil(layersizes(i)*rand(1,numconn));
+            Wtmp{i}(j,idx) = randn(numconn,1)*initcoeff;
+        end
+    end
+    paramsp = pack(Wtmp, btmp);
+else
+    paramsp = paramsinit;
+end
+
+% compute gradient of loss function.
+function grad = calcu_grad(paramsp)
+    [Wu, bu] = unpack(paramsp);
+    y = cell(1, numlayers+1);
+    
+    %forward prop:
+    y{1, 1} = indata(:, 1:numcases );
+    yip1 =  y{1, 1} ;
+    dEdW = cell(numlayers, 1);
+    dEdb = cell(numlayers, 1);
+    for i = 1:numlayers
+        yi = yip1;
+        xi = Wu{i}*yi + repmat(bu{i}, [1 numcases]);
+        if strcmp(layertypes{i}, 'logistic')
+            yip1 = 1./(1 + exp(-xi));
+        elseif strcmp( layertypes{i}, 'softmax' )
+            tmp = exp(xi);
+            yip1 = tmp./repmat( sum(tmp), [layersizes(i+1) 1] );
+        end
+        y{1, i+1} = yip1;
+    end
+    % TODO:
+    eval_f = eval_f + 1;
+    outc = outdata(:, 1:numcases );
+    
+    for i = numlayers:-1:1
+        if i < numlayers
+            if strcmp(layertypes{i}, 'logistic')
+                dEdxi = dEdyip1.*yip1.*(1-yip1);
+            end
+        else
+            dEdxi = outc - yip1; %simplified due to canonical link
+        end
+        dEdyi = Wu{i}'*dEdxi;
+        yi = y{1, i};
+
+        %standard gradient comp:
+        dEdW{i} = dEdxi*yi';
+        dEdb{i} = sum(dEdxi,2);
+        dEdyip1 = dEdyi;
+        yip1 = yi;
+    end
+    
+    eval_g = eval_g + 1;
+
+    % psize x 1
+    grad = pack(dEdW, dEdb);
+    grad = grad / numcases;
+    grad = grad - weight_decay*(paramsp);
+    
+    % NOTE: grad is the gradient w.r.t. loss other than Log-likelihood.
+    grad = -grad;
+end
+
+times = zeros(maxIter,1);
+outputString(sprintf('================ Start Training for %d iters... ================', maxIter))
+% Main part: train and test.
+
+gamma = 0.9;
+
+m = size(paramsp,1);
+v = zeros(m,1);
+
+[ll, err] = computeLL(paramsp, indata, outdata);
+llrecord(1,1) = ll;
+errrecord(1,1) = err;
+outputString( ['Train Log likelihood: ' num2str(ll) ', error rate: ' num2str(err)] );
+
+eval_gs(1,1) = eval_g;
+eval_fs(1,1) = eval_f;
+step_size(1,1) = 0;
+
+
+[ll_test, err_test] = computeLL(paramsp, intest, outtest);
+llrecord(1,2) = ll_test;
+errrecord(1,2) = err_test;
+outputString( ['Test Log likelihood: ' num2str(ll_test) ', error rate: ' num2str(err_test)] );
+outputString( '' );
+
+grad = calcu_grad(paramsp);
+
+lbfgs_m = 7;
+bfgs_s = [];
+bfgs_y = [];
+
+for epoch = 1:maxIter
+    tic
+    bfgs_q = -grad;
+    bfgs_p = bfgs_q;
+    if epoch ~= 1
+        alpha = zeros(1,lbfgs_m);
+        for i = size(bfgs_s,2):-1:1
+            alpha(i) = bfgs_s(:,i)'*bfgs_q / (bfgs_y(:,i)'*bfgs_s(:,i));
+            bfgs_q = bfgs_q - alpha(i)*bfgs_y(:,i);
+        end
+        H0 = bfgs_y(:,end)'*bfgs_s(:,end) / (bfgs_y(:,end)'*bfgs_y(:,end));
+        bfgs_p = H0*bfgs_q;
+        for i = 1:size(bfgs_s,2)
+            beta = bfgs_y(:,i)'*bfgs_p / (bfgs_y(:,i)'*bfgs_s(:,i));
+            bfgs_p = bfgs_p + (alpha(i) - beta)*bfgs_s(:,i);
+        end
+    end
+    
+    if strcmp(algorithm, 'gradient descent')
+        % backtracking...
+        outputString(['GD w/ backtracking, trial: ', num2str(params.trial), ', epoch: ',num2str(epoch)]);
+        step = eta;
+        v = -grad;
+        
+        % backtracking line search
+        j = 0;
+        oldll = ll;
+        [ll, err] = computeLL(paramsp + step*v, indata, outdata);
+        while j < 60
+            if -ll <= -oldll - 0.01*step*v'*v
+                break;
+            else
+                step = 0.8*step;
+                j = j + 1;
+                [ll, err] = computeLL(paramsp + step*v, indata, outdata);
+            end
+        end
+        paramsp = paramsp + step*v;
+        outputString( ['# Backtracking: ' num2str(j) ', step size: ' num2str(step)] );
+    elseif strcmp(algorithm, 'fixstep-lbfgs') % fixstep lbfgs
+        outputString(['Fixstep L-BFGS, trial: ', num2str(params.trial), ', epoch: ',num2str(epoch)]);
+        % +/- bug?
+        v = eta*bfgs_p;
+        paramsp = paramsp - v;
+        outputString( ['No backtracking, step size: ' num2str(eta)] );
+    elseif strcmp(algorithm, 'momentum-lbfgs')
+        outputString(['Momentum L-BFGS, trial: ', num2str(params.trial), ', epoch: ',num2str(epoch)]);
+        v = gamma*v + eta*bfgs_p;
+        paramsp = paramsp - v;
+        outputString( ['No backtracking, step size: ' num2str(eta)] );
+    end
+
+    [ll, err] = computeLL(paramsp, indata, outdata);
+    
+    bfgs_s = [bfgs_s, v];
+    
+    gradold = grad;
+    grad = calcu_grad(paramsp);
+    
+
+    bfgs_y = [bfgs_y, grad - gradold];
+
+    if size(bfgs_s,2) > lbfgs_m
+        bfgs_s = bfgs_s(:,2:end);
+        bfgs_y = bfgs_y(:,2:end);
+    end
+
+    
+    
+    
+    
+    
+    
+    
+    %Parameter update:
+    llrecord(epoch+1,1) = ll;
+    errrecord(epoch+1,1) = err;
+    outputString( ['Train Log likelihood: ' num2str(ll) ', error rate: ' num2str(err)] );
+
+    eval_gs(epoch+1,1) = eval_g;
+    eval_fs(epoch+1,1) = eval_f;
+    step_size(epoch+1,1) = norm(v);
+
+    [ll_test, err_test] = computeLL(paramsp, intest, outtest);
+    llrecord(epoch+1,2) = ll_test;
+    errrecord(epoch+1,2) = err_test;
+    outputString( ['Test Log likelihood: ' num2str(ll_test) ', error rate: ' num2str(err_test)] );
+    outputString( '' );
+
+    times(epoch) = toc;
+end
+
+outputString( ['Total time: ' num2str(sum(times)) ] );
+end
+
+%EOF.
